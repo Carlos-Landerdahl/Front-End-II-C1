778c4d5fb100c7537b06ba1e6659bd0c5d391898
charlim <carloslanderdahl27@gmail.com>
2022-11-08 20:45:18 -0300

    docs: deploying inputs/labels

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let sendButton = document.getElementById('sendButton');
+
+sendButton.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    let nick = document.getElementById('nick');
+    let content = document.getElementById('content');
+    let img = document.getElementById('img');
+    let number = document.getElementById('number');
+
+
+    console.log(nick.value);
+
+    
+})
