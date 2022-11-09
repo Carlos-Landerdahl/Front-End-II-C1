aea9ff7423631a356946523eafc872a05f5108e1
gutolive09 <gutolive01@gmail.com>
2022-11-09 16:31:37 -0300

    added card building logic

--- scripts.js
+++ scripts.js
@@ -8,8 +8,21 @@ sendButton.addEventListener('click', function (e) {
     let img = document.getElementById('img');
     let number = document.getElementById('number');
 
-
     console.log(nick.value);
 
+    let containerCards = document.getElementById('container-cards')
     
+    let card = document.createElement("div")
+
+    card.innerHTML =  `<img src="${img.value}"></img>
+    <div class="card-text">
+    <h3>${nick.value}</h3>
+    <p>${content.value}</p>
+    <span>Número da camiseta:</span>
+    <span>${number.value}</span>
+    </div>`
+
+    card.classList.add("card")
+    
+    containerCards.appendChild(card)
 })
