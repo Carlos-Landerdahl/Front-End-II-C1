f14890f23dd31d0b465df5f2716824fa490f2463
charlim <carloslanderdahl27@gmail.com>
2022-11-10 16:44:01 -0300

    docs: style cards

--- scripts.js
+++ scripts.js
@@ -11,20 +11,20 @@ function validator() {
         content.value.length > 4 && content.value !== '' &&
         img.value.length > 4 && img.value !== '' &&
         number.value.length > 0 && number.value !== ''
-    ){
+    ) {
         sendButton.disabled = false;
-    }else {
+    } else {
         sendButton.disabled = true;
     }
 }
 
-nick.addEventListener("input", function(e){
+nick.addEventListener("input", function (e) {
     smallNick.style.display = "block"
     validator()
 })
 
-content.addEventListener("input", function(e){
-    validator() 
+content.addEventListener("input", function (e) {
+    validator()
 })
 
 img.addEventListener("input", function (e) {
@@ -43,22 +43,21 @@ sendButton.addEventListener('click', function (e) {
     console.log(nick.value);
 
     let containerCards = document.getElementById('container-cards')
-    
+
     let card = document.createElement("div")
 
-    card.innerHTML =  `<img src="${img.value}"></img>
-    <div class="card-text">
-    <h3>${nick.value}</h3>
+    card.innerHTML = `
+    <div class="card-content">
+    <img "card-img" src="${img.value}"></img>
+    <h3>${nick.value}<span>${number.value}</span></h3>
     <p>${content.value}</p>
-    <span>Número da camiseta:</span>
-    <span>${number.value}</span>
     </div>`
 
-    card.classList.add("card") 
-    
+    card.classList.add("card")
+
     containerCards.appendChild(card)
 
-    if (nick.value.length > 0){
+    if (nick.value.length > 0) {
         document.getElementById("sendButton").disabled = false;
     }
 })
