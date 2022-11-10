9d4413bd9e5c6733a018b65ae4f16eb24c0078c4
charlim <carloslanderdahl27@gmail.com>
2022-11-09 21:33:28 -0300

    docs: validation on inputs

--- scripts.js
+++ scripts.js
@@ -4,16 +4,50 @@ let content = document.getElementById('content');
 let img = document.getElementById('img');
 let number = document.getElementById('number');
 
+sendButton.disabled = true;
+function validator() {
+    if (
+        nick.value !== null && nick.value !== '' &&
+        content.value !== null && content.value !== '' &&
+        img.value !== null && img.value !== '' &&
+        number.value !== null && number.value !== ''
+    ){
+        sendButton.disabled = false;
+    }else {
+        sendButton.disabled = true;
+    }
+}
+
+nick.addEventListener("input", function(e){
+    validator()
+})
+
+content.addEventListener("input", function(e){
+    validator()
+})
+
+img.addEventListener("input", function (e) {
+    validator()
+})
+
+number.addEventListener("input", function (e) {
+    validator()
+})
+
+let smallNick = document.getElementById("smallNick")
+
+nick.addEventListener("keyup", function () {
+    if (nick.value.length < 3) {
+        smallNick.style.display = "block"
+    }
+    else {
+        smallNick.style.display = "none"
+    }
+
+})
+
+
 sendButton.addEventListener('click', function (e) {
-    if(nick.value=="" || 
-    content.value=="" ||
-    img.value==""||
-    number.value==""){
-        sendButton.setAttribute("disabled", true)
-    }
-    else{
-        sendButton.removeAttribute("disabled")
-    }
     e.preventDefault()
 
     console.log(nick.value);
@@ -33,17 +67,9 @@ sendButton.addEventListener('click', function (e) {
     card.classList.add("card") 
     
     containerCards.appendChild(card)
-})
 
-let smallNick = document.getElementById("smallNick")
-
-nick.addEventListener("keyup", function(){
-    if(nick.value.length < 3){
-        smallNick.style.display="block"
+    if (nick.value.length > 0){
+        document.getElementById("sendButton").disabled = false;
     }
-    else {
-        smallNick.style.display="none"
-    }
-    
 })
 
