0b408f0ada7d78ee3bc7845cc75b6ca479baac3b
gutolive09 <gutolive01@gmail.com>
2022-11-14 23:47:00 -0300

    docs: Fix validation/design

--- scripts.js
+++ scripts.js
@@ -19,23 +19,55 @@ function validator() {
 }
 
 nick.addEventListener("input", function (e) {
-    smallNick.style.display = "block"
     validator()
+    if(nick.value.length > 2 && nick.value !== ''){
+        smallNick.style.display = "none" 
+    }
+    else {
+        smallNick.style.display = "block"
+        
+    }
 })
 
 content.addEventListener("input", function (e) {
     validator()
+    if(content.value.length > 4 && content.value !== ''){
+        smallContent.style.display = "none" 
+    }
+    else {
+        smallContent.style.display = "block"
+        
+    }
 })
 
 img.addEventListener("input", function (e) {
     validator()
+    if(img.value.length > 4 && img.value !== ''){
+        smallImg.style.display = "none" 
+    }
+    else {
+        smallImg.style.display = "block"
+        
+    }
 })
 
 number.addEventListener("input", function (e) {
     validator()
+    if(number.value.length > 0 && number.value !== ''){
+        smallNumber.style.display = "none" 
+    }
+    else {
+        smallNumber.style.display = "block"
+    
+    }
 })
 
+
+
 let smallNick = document.getElementById("smallNick")
+let smallContent = document.getElementById("smallContent")
+let smallImg = document.getElementById("smallImg")
+let smallNumber = document.getElementById("smallNumber")
 
 sendButton.addEventListener('click', function (e) {
     e.preventDefault()
@@ -48,7 +80,7 @@ sendButton.addEventListener('click', function (e) {
 
     card.innerHTML = `
     <div class="card-content">
-    <img "card-img" src="${img.value}">
+    <div class="card-image" id="card-image" style="background-image:url(${img.value})"></div>
     <img class="card-icon" src="https://cdn-icons-png.flaticon.com/512/5974/5974771.png" alt="">
     <h3>${nick.value}<span>${number.value}</span></h3>
     <p>${content.value}</p>
@@ -62,4 +94,3 @@ sendButton.addEventListener('click', function (e) {
         document.getElementById("sendButton").disabled = false;
     }
 })
-
